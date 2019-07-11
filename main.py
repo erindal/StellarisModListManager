ff0561f3a1ce0f4c0218fe5ceb6c113c23932871
erindalc <e.cuthalion@gmail.com>
2019-07-11 00:14:34 -0400

    decompile and compile settings
    for ease of editing the mod list

--- main.py
+++ main.py
@@ -5,13 +5,23 @@
 
 import setup
 import gui
-
+import util
 
 def run():
 	doRun = setup.confirmStellaris()
 	if doRun:
 		gui.start()
 		
+		
+def dev():
+	setup.confirmStellaris()
+	test = util.readSettingsFile()
+	testDict = util.decompileSettings(test)
+	testDict["Mods"] = "\n"
+	testOut = util.compileSettings(testDict)
+	util.writeSettingsFile(testOut)
+		
 if __name__ == "__main__":
    # stuff only to run when not called via 'import' here
-   run()
+   #run()
+   dev()
--- util.py
+++ util.py
@@ -7,9 +7,9 @@ import setup
 
 def readSettingsFile():
 	settings_file = open(setup.settings_path, 'r')
-	filestring = settings_file.read()
+	fileString = settings_file.read()
 	settings_file.close()
-	return filestring
+	return fileString
 	
 	
 def writeSettingsFile(stringtowrite):
@@ -17,3 +17,22 @@ def writeSettingsFile(stringtowrite):
 	settings_file.write(stringtowrite)
 	settings_file.close()
 	print("Wrote to settings file")
+	
+def decompileSettings(settingsString):
+	settingsDict = {}
+	
+	index1 = settingsString.find("last_mods={") #finds beginning of mod list
+	
+	index2 = settingsString.find("}\nautosave") #finds end of mod list
+	
+	settingsDict["Beginning"] = settingsString[0:index1+11] #first part of settings file
+	
+	settingsDict["Mods"] = settingsString[index1+12:index2-1] #this is the indented mod list
+	
+	settingsDict["End"] = settingsString[index2:] #second part of settings file
+	
+	return settingsDict
+	
+def compileSettings(settingsDict):
+	settingsString = settingsDict["Beginning"] + settingsDict["Mods"] + settingsDict["End"]
+	return settingsString
