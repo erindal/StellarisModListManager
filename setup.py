f172c459cfb0a8b5c0115a04968d5144e3ab76ed
erindalc <e.cuthalion@gmail.com>
2019-07-10 23:51:15 -0400

    read and write funcs

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ import getpass
 import os
 
 currentuser = getpass.getuser()
+settings_path = "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/settings.txt"
 
-def confirmStellaris():
+def confirmStellaris(): #returns false if no stellaris dir exists or if cannot create modlist dir
 
 	#Ensure settings file exists
 	if not os.path.exists("C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/settings.txt"):
@@ -18,13 +19,14 @@ def confirmStellaris():
 		print("Stellaris folder detected.")
 		
 		#Create saved folder directory	
-		if not os.path.exists("C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SavedModLists"):
-			path = "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SavedModLists"
+		if not os.path.exists("C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SMLM"):
+			path = "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SMLM"
 			try:
 				os.mkdir(path)
 			except:
 				print("Failed to create mod list directory. Check permissions!")
 				print(path)
+				return False
 			else:
 				print("Mod list directory created.")
 	
--- util.py
+++ util.py
@@ -1,3 +1,19 @@
 #Author: Erindal
 #Contact: erindalc@gmail.com
 #This code is under the MIT License, however if you use it, some notification would be appreciated!
+
+import setup
+
+
+def readSettingsFile():
+	settings_file = open(setup.settings_path, 'r')
+	filestring = settings_file.read()
+	settings_file.close()
+	return filestring
+	
+	
+def writeSettingsFile(stringtowrite):
+	settings_file = open(setup.settings_path, 'w')
+	settings_file.write(stringtowrite)
+	settings_file.close()
+	print("Wrote to settings file")
