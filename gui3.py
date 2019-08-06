42dfa76166d74e187146a47e451c8449b47bf45e
erindalc <e.cuthalion@gmail.com>
2019-08-06 04:09:09 -0400

    semi functional modlist ui

--- gui3.py
+++ gui3.py
@@ -3,6 +3,7 @@
 # This code is under the MIT License, however if you use it, some notification would be appreciated!
 
 from appJar import gui
+from time import sleep
 import util
 import setup
 
@@ -18,66 +19,115 @@ class State:
 	modString = ""
 	currentProfile = ""
 	isSaved = False
+	isActivated = False
+	selectedModNames = []
+
+
+class StringMod:  # This holds the variables added onto mod names to separate them, for when a mod name conflicts
+	right = "-RIGHT"
+	left = "-LEFT"
+
 
 # Functions
 
 # Status Function - Call to update status bar
-# TODO
+def updateStatus():
+	pass
 
+def runThreadedUpdate():
+	sleep(2)
+	app.thread(threadedUpdate)
 
-# Update mods in mod list and all mods
-# This should be set as the check box function
-def updateModlist(): #TODO CONVERT NAMELIST TO PROPER MOD NAMES
+def updateModLists(modDict):
+	for i in modDict:
+		if modDict[i]:
+			if i not in State.selectedModNames:
+				State.selectedModNames.append(i)
+		else:
+			if i in State.selectedModNames:
+				State.selectedModNames.remove(i)
 
-	#Get selected mod
+def threadedUpdate():
+	modDict = app.getAllCheckBoxes()
+	app.queueFunction(clearAllPanes)
+	app.queueFunction(updateModLists, modDict)
+	app.queueFunction(populateModList, State.selectedModNames)
+	app.queueFunction(populateSelectedMods)
+
+# Update Mod UI - Call to update the mod list in application
+def updateModList():
+	modDict = app.getAllCheckBoxes()
+	clearAllPanes()
+	#print(modDict)
+	for i in modDict:
+		if modDict[i]:
+			if i not in State.selectedModNames:
+				State.selectedModNames.append(i)
+		else:
+			if i in State.selectedModNames:
+				State.selectedModNames.remove(i)
+
+	print(State.selectedModNames)
+
+	populateModList(ignore=State.selectedModNames)
+	populateSelectedMods()
+
+# Clear both scroll panes to avoid conflicts
+def clearAllPanes():
 	app.openFrame("LEFT")
-	app.openScrollPane("All Mods")
+	app.openScrollPane("Available Mods")
+	app.emptyCurrentContainer()
+	app.stopScrollPane()
+	app.stopFrame()
+	app.openFrame("RIGHT")
+	app.openScrollPane("Current Mods")
+	app.emptyCurrentContainer()
+	app.stopScrollPane()
+	app.stopFrame()
 
-	nameList = util.boxDictToNameList(app.getAllCheckBoxes())
+# Populate Mod UI - Run after to reset the mod list in application
+def populateModList(ignore=[]):
+	app.openFrame("LEFT")
+	app.openScrollPane("Available Mods")
+
+	app.emptyCurrentContainer()
+
+	for i in allMods:
+		if i.name not in ignore:
+			try:
+				app.addCheckBox(i.name)
+				app.setCheckBoxChangeFunction(i.name, runThreadedUpdate)
+			except:
+				print("Skipping " + i.name)
 
 	app.stopScrollPane()
 	app.stopFrame()
 
-	# Get existing mods, update right
+def populateSelectedMods():
 	app.openFrame("RIGHT")
-
 	app.openScrollPane("Current Mods")
 
-	currentModList = util.boxDictToNameList(app.getAllCheckBoxes()) + nameList
-	currentModList.sort()
+	#app.emptyCurrentContainer()
 
-	app.emptyCurrentContainer()
-
-	for i in currentModList:
+	for i in State.selectedModNames:
 		app.addCheckBox(i)
-		app.setCheckBoxChangeFunction(i, updateModlist)
+		app.setCheckBox(i, ticked=True)
+		app.setCheckBoxChangeFunction(i, runThreadedUpdate)
 
 	app.stopScrollPane()
 	app.stopFrame()
-
-	# Update left side
-	app.openFrame("LEFT")
-
-	app.openScrollPane("All Mods")
-
-	app.emptyCurrentContainer()
-	populateMods(currentModList)
-
-	app.stopScrollPane()
-	app.stopFrame()
-
-
 
 
 # Menu Buttons
 def menuButtons(button):
 	if button == "Create New Profile":
-		createProfile()
+		populateModList()
+		#createProfile()
 	elif button == "Change Profile":
-		pass
-	elif button == "Share":
-		pass
-	elif button == "Export":
+		changeProfile()
+	elif button == "Export/Share":
+		shareMenu()
+	elif button == "Import":
 		pass
 	elif button == "Activate Profile":
 		pass
@@ -88,7 +138,7 @@ def menuButtons(button):
 
 # Create Profile
 def createProfile():
-	populateMods()
+	pass
 
 # Change Profile
 def changeProfile():
@@ -118,21 +168,6 @@ def importMenu():
 def exportMenu():
 	pass
 
-# Update mod list
-def populateMods(notInclude=[]):
-	app.openFrame("LEFT")
-
-	app.openScrollPane("All Mods")
-
-	for i in allMods:
-		if i.name not in notInclude:
-			app.addNamedCheckBox(i.name, i.name+"LEFT")
-			app.setCheckBoxChangeFunction(i.name+"LEFT", updateModlist)
-
-	app.stopScrollPane()
-
-	app.stopFrame()
-
 
 # APP CONFIG
 app.setBg("#6B7A8F")
@@ -145,11 +180,10 @@ app.setStatusbar("Not Activated", 1)
 
 # Menu bar
 fileMenus = ["Settings", "Quit"]
-profileMenus = ["Create New Profile", "Change Profile", "-", "Share", "Export", "-", "Activate Profile"]
+profileMenus = ["Create New Profile", "Change Profile", "-", "Import", "Export/Share", "-", "Activate Profile"]
 
 app.addMenuList("File", fileMenus, menuButtons)
 app.addMenuList("Profile", profileMenus, menuButtons)
-
 
 
 # Frames
@@ -166,7 +200,11 @@ app.stopFrame()
 app.startFrame("LEFT", row=1, column=0)
 app.setBg("white")
 
-app.startScrollPane("All Mods")
+app.startScrollPane("Available Mods")
+# temporary fixes toward forcing a larger size
+app.setScrollPaneHeight("Available Mods", 500)
+app.setScrollPaneSticky("Available Mods", "both")
+app.addLabel("None loaded")
 
 app.stopScrollPane()
 
@@ -176,16 +214,14 @@ app.stopFrame()
 app.startFrame("RIGHT", row=1, column=1)
 app.setBg("white")
 
+
 app.startScrollPane("Current Mods")
+app.addLabel("None enabled")
 
 app.stopScrollPane()
 
 app.stopFrame()
 
-#Checkbox setup
-
-
-
-#RUN APP
+# RUN APP
 def runApp():
 	app.go()
