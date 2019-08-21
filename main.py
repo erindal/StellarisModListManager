8fa9b3f003a7f11c2581df8bd88e3c2038ab0790
erindalc <e.cuthalion@gmail.com>
2019-08-21 06:58:07 -0400

    2.0 PR (#7)
    Complete ground up rewrite.

--- main.py
+++ main.py
@@ -1,190 +1,21 @@
-#Author: Erindal
-#Contact: erindalc@gmail.com
-#This code is under the MIT License, however if you use it, some notification would be appreciated!
+# Author: Erindal
+# Contact: erindalc@gmail.com
+# This code is under the MIT License, however if you use it, some notification would be appreciated!
 
 
-import getpass
-import os
-import shutil
+import setup
 import gui
+import util
+import gui3
 
-currentuser = getpass.getuser()
 
-def main():
+def run():
+    doRun = setup.confirmStellaris()
+    if doRun:
+        #gui.start()
+        gui3.runApp()
 
-	#Ensure settings file exists
-	if not os.path.exists("C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/settings.txt"):
-		print("You must run Stellaris at least once before using this program. Exiting...")
-		
-	else:
-		print("Stellaris folder detected.")
-		
-		#Create saved folder directory	
-		if not os.path.exists("C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SavedModLists"):
-			path = "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SavedModLists"
-			try:
-				os.mkdir(path)
-			except:
-				print("Failed to create mod list directory. Check permissions!")
-			else:
-				print("Mod list directory created.")
-	
-		else:
-			print("Mod list directory found.")
-	
-	
-		
-		print("!!")
-		print("Warning, this tool will copy your ENTIRE settings file.")
-		print("This means if you make changes to your ingame settings, then load a mod list, those changes will be lost.")
-		print("!!")
-		
-		selection()
-		
-def selection():
-	print("Would you like to load or save a modlist? Type 'load' or 'save'")
-	print("To read the current mod list, type 'read'")
-	print("To use the gui beta, type 'gui'")
-	saveorload = input("> ")
-	
-	if saveorload == 'load':
-		load_modlist()
-		
-	elif saveorload == 'save':
-		save_modlist()
-		
-	elif saveorload == 'read':
-		read_modlist()
-		
-	elif saveorload == 'dev':
-		dev()
-		
-	elif saveorload == 'gui':
-		gui.main()
-		
-	else:
-		print("Invalid command. Try again. To quit close this window.")
-		selection()
-		return
-	
-def read_modlist(): #Prints mod names of current mod list
-	settings_path = "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/settings.txt"
 
-	settings_file = open(settings_path, 'r')
-	
-	#Cleans file and makes it ready to read
-	filestring = settings_file.read() 
-	filestring = filestring.replace('\n', ',')
-	filestring = filestring.replace('\t', '')
-	filesplit = filestring.split(',')
-	
-	modfilelist = []
-	modlist = []
-	found = False
-	
-	#Get mod list
-	for i in filesplit:
-		#if found is false, program searches for beginning of mod list string
-		if found is False:
-			if i == 'last_mods={':
-				print('Found mod list, parsing')
-				print("")
-				found = True
-		else:	#once found is true, each entry is added to the modlist.
-			if i != '}':
-				modlist.append(i)
-			else:
-				found = False
 
-	#strip extra quotes
-	for i in modlist:
-		#print(i)
-		i = i.replace('"','')
-		i = i.replace('mod/','')
-		modfilelist.append(i)
-
-	#Gets the mod title from the mod file
-	for i in modfilelist:
-		currentmodfile = "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/mod/" + i
-		try:
-			tempmodfile = open(currentmodfile, 'r')
-		except:
-			print("!!")
-			print("An invalid mod id exists. The mod may not have downloaded, or many not exist. ID: " + i + ". Exiting")
-			return
-		
-		lineone = tempmodfile.readline()
-		modname = lineone.replace('name="','')
-		modname = modname.replace('"\n','')
-		print(modname)
-		
-	#Run again	
-	print("")
-	selection()
-		
-def dev():
-	print()
-		
-def load_modlist():
-	#Make backup
-	print("Making backup")
-	shutil.copyfile("C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/settings.txt", "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SavedModLists/_settings_backup.txt")
-
-	#Get list of mod lists
-	path = "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SavedModLists"
-	filelist = os.listdir(path)
-	print("The following mod lists are available: ")
-	print(filelist)
-	print("Please enter the full file name of the modlist you want to load (Do not include .txt):")
-	listtoload = input("> ")
-	
-	#Copy and replace settings file
-	try:
-		shutil.copyfile("C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SavedModLists/" + listtoload + ".txt", "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/settings.txt")
-	except FileNotFoundError:
-		print("File doesn't not exist. Please check your spelling and try again.")
-	else:
-		print("Mod list loaded.")
-		
-	#Run again
-	print("")
-	selection()
-	
-def save_modlist():
-	print("Please enter a name for your modlist. You may wish to include a Stellaris version number as well")
-	savename = input("> ")
-	
-	newfilepath = ("C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SavedModLists/" + savename + ".txt")
-	
-	#Check if file already exists
-	if os.path.isfile(newfilepath) is True:
-		print("File already exists, Y or N for overwrite")
-		dooverwrite = input("> ")
-		
-		#confirm overwrite or abort
-		if dooverwrite == "Y":
-			try:
-				shutil.copyfile("C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/settings.txt", "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SavedModLists/" + savename + ".txt")
-			except:
-				print("An unknown error occured. Mod list not saved.")
-			else:
-				print("File saved")
-		
-		else:
-			print("Mod list not saved.")
-	
-	#Copy the file	
-	else:
-		try:
-			shutil.copyfile("C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/settings.txt", "C:/Users/" + currentuser + "/Documents/Paradox Interactive/Stellaris/SavedModLists/" + savename + ".txt")
-		except: #Known errors: when using bad characters such as "?"
-			print("An unknown error occured. Mod list not saved.")
-		else:
-			print("File saved")
-
-	print("")
-	selection()
-	
 if __name__ == "__main__":
-   # stuff only to run when not called via 'import' here
-   main()
+    run()
---
+++ util.py
@@ -0,0 +1,182 @@
+# Author: Erindal
+# Contact: erindalc@gmail.com
+# This code is under the MIT License, however if you use it, some notification would be appreciated!
+
+import setup
+import os
+
+
+class Mod:  # holds mod info
+	def __init__(self, name, path):
+		self.name = name
+		self.path = path
+
+def readSettingsFile():  # returns settings file string
+	settings_file = open(setup.settings_path, 'r')
+	fileString = settings_file.read()
+	settings_file.close()
+	return fileString
+
+
+def writeSettingsFile(stringtowrite):  # takes string, writes to settings file
+	settings_file = open(setup.settings_path, 'w')
+	settings_file.write(stringtowrite)
+	settings_file.close()
+	print("Updated settings file")
+
+
+def decompileSettings(settingsString):  # takes settings file string, returns separated dictionary of settings file
+	settingsDict = {}
+
+	index1 = settingsString.find("last_mods={")  # finds beginning of mod list
+	# TODO HANDLE THIS NOT EXISTING
+	index2 = settingsString.find("}\nautosave")  # finds end of mod list
+
+	settingsDict["Beginning"] = settingsString[0:index1 + 11]  # first part of settings file
+
+	settingsDict["Mods"] = settingsString[index1 + 12:index2 - 1]  # this is the indented mod list
+
+	settingsDict["End"] = settingsString[index2:]  # second part of settings file
+
+	return settingsDict
+
+
+def compileSettings(settingsDict):  # takes settings dictionary, returns complete string for settings file
+	settingsString = settingsDict["Beginning"] + settingsDict["Mods"] + settingsDict["End"]
+	return settingsString
+
+
+def readMods(settingsDict):  # takes settings dict returns list of mod name strings
+	modString = settingsDict["Mods"]
+	modString = modString.replace("\t", "")  # strip tabs
+	modList = modString.split("\n")  # separate to list
+	modNameList = []
+
+	for i in modList:
+		modNameList.append(modPathToName(i))
+
+	return modNameList
+
+
+def modPathToName(modPath):  # takes mod path and returns the mod name as a string
+	# strip quotes
+	modPath = modPath.replace('"', '')
+
+	currentmodfile = "C:/Users/" + setup.currentuser + "/Documents/Paradox Interactive/Stellaris/" + modPath
+	try:
+		tempmodfile = open(currentmodfile, 'r')
+	except FileNotFoundError:
+		print("An invalid mod path exists!")
+		print(tempmodfile)
+		return
+	except PermissionError:
+		print("Permission Error")
+		print(tempmodfile)
+		print("SMLM does not have access to this file.")
+		return
+
+	# TODO - search for name=" instead
+	lineone = tempmodfile.readline()
+	modname = lineone.replace('name="', '')
+	modname = modname.replace('"\n', '')
+	return modname
+
+
+def getAllMods():  # returns list of Mod classes
+	currentdir = setup.mod_folder_path
+
+	allModsList = []
+
+	for filename in os.listdir(currentdir):  # go through mod directory
+		if filename[-4:] == ".mod":  # to ignore mod directories
+			try:
+				tempfile = open(currentdir + filename, 'r')
+
+				# clean mod name
+				lineone = tempfile.readline()
+				modname = lineone.replace('name="', '')
+				modname = modname.replace('"\n', '')
+
+				# create class with name and path
+				allModsList.append(Mod(name=modname, path=filename))
+
+			except PermissionError:
+				print("Permission Error")
+				print(filename)
+				print("SMLM does not have access to this file.")
+
+	return allModsList
+
+
+def sortModList(val):  # use as a key
+	return val.name
+
+
+def listModNames(modObjList):
+	for i in modObjList:
+		print(i.name)
+
+
+# check boxes to path funcs
+def boxDictToNameList(boxDict):
+	nameList = []
+
+	for i in boxDict:
+		if boxDict[i] == True:
+			nameList.append(i)
+
+	return nameList
+
+
+def nameListToPathList(nameList, allMods):
+	pathList = []
+
+	for i in allMods:
+		if i.name in nameList:
+			pathList.append(i.path)
+
+	return pathList
+
+
+def pathListToString(pathList):
+	pathString = "\n"
+
+	for i in pathList:
+		pathString = pathString + '\t"mod/' + i + '"\n'
+
+	return pathString
+
+
+def getAllProfiles():
+	currentdir = setup.save_folder_path
+	profileList = []
+
+	for filename in os.listdir(currentdir):  # go through save directory
+		if filename[-4:] == ".txt":  # to ignore sub directories
+			profileList.append(filename[:-4])
+			#print(filename)
+
+	return profileList
+
+
+def parseSavedProfile(profile, modDict):  #returns list of mod names
+	file = open(setup.save_folder_path + profile + ".txt", "r")
+	modString = file.read()
+
+	returnlist = cleanModString(modString, modDict)
+	return returnlist
+
+def cleanModString(modString, modDict):
+	tempString = modString.replace('\t"mod/', "")  # clear formatting
+	tempString = tempString.replace('"', "")  # clear quote
+	templist = tempString.split("\n")  # Split by newline
+	returnlist = []
+	for path in templist:
+		if path == "":
+			pass
+		else:
+			for mod in modDict:
+				if mod.path == path:
+					returnlist.append(mod.name)
+
+	return returnlist
